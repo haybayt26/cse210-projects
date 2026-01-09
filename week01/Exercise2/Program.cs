c4638a0742da01841e2d67162157d9bb76e83f4f
Haley Light <haybayt26@gmail.com>
2026-01-08 22:57:42 -0700

    Exercise 2 Complete

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade? ");
+        string strGrade = Console.ReadLine();
+        int intGrade = int.Parse(strGrade);
+        string letter = "";
+        if (intGrade >= 90)
+        {
+            letter = "A";
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else if (intGrade < 90 && intGrade >= 80)
+        {
+            letter = "B";
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else if (intGrade < 80 && intGrade >= 70)
+        {
+            letter = "C";
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else if (intGrade < 70 && intGrade >= 60)
+        {
+            letter = "D";
+            Console.WriteLine("Better luck next time!");
+        }
+        else
+        {
+            letter = "F";
+            Console.WriteLine("Better luck next time!");
+        }
+        Console.WriteLine($"For {intGrade}% you get a {letter}");
+
     }
 }
