87afc3aee537f528055e47aad97fb845574e256b
Haley Light <haybayt26@gmail.com>
2026-01-08 23:47:49 -0700

    Exercise 3 Complete

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Xml.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool wantToPlay = false;
+        int rounds = 0;
+        do
+        {
+            Console.Write("Magic Number Generated between 1-20");
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1,20);
+            bool correct = false;
+            int tries = 0;
+
+            do
+            {
+                Console.WriteLine("What is your guess? ");
+                string strGuess = Console.ReadLine();
+                int guess = int.Parse(strGuess);
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess == number)
+                {
+                    Console.WriteLine("Correct!");
+                    correct = true;
+                }
+                tries += 1;
+
+            } while (correct == false);
+
+            Console.WriteLine($"Congrats! It took you {tries} tries.");
+            Console.WriteLine($"Want to play again? (y/n) ");
+            string choice = Console.ReadLine();
+            if (choice == "y")
+            {
+                wantToPlay = true;
+            }
+            else if (choice == "n")
+            {
+                wantToPlay = false;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect Input");
+                wantToPlay = false;
+            }
+            rounds += 1;
+
+        } while (wantToPlay == true);
+
+        Console.WriteLine($"You played {rounds} round(s), Goodbye.");
     }
+    
 }
