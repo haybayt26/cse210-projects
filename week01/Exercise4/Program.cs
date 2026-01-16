350f10c48a7ec7b6ef32f5a54250906eb32608b5
Haley Light <haybayt26@gmail.com>
2026-01-15 20:51:18 -0700

    Exercise 4 Completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Xml.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int sum = 0;
+        float average = 0;
+        int largest = 0;
+        bool play = true;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        while (play == true)
+        {
+            Console.Write("Enter number: ");
+            string entryStr = Console.ReadLine();
+            int entryInt = int.Parse(entryStr);
+            numbers.Add(entryInt); 
+
+            if (entryInt > largest)
+            {
+                largest = entryInt;
+            }
+            if (entryInt == 0)
+            {
+                play = false;
+            }
+        }
+
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        average = (float)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
