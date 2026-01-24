96031a7ebfe7d65b9ca1295b774072b98177bdf1
Haley Light <haybayt26@gmail.com>
2026-01-24 15:22:49 -0700

    Recommiting for Grading

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -13,10 +13,10 @@ public class Entry
     public static void GeneratePrompts()
     {
         _prompts.Add("What went well today, and why?");
-        _prompts.Add("What's something I'm currently struggling with, and what's one small step I could take to improve it?");
+        _prompts.Add("What's something I'm currently struggling with, and what's one small thing I could take to improve it?");
         _prompts.Add("What did I learn recently (about myself or someone eles)?");
         _prompts.Add("What am I avoiding right now, and what am I afraid would happen if I faced it?");
-        _prompts.Add("Who was the most interesting person I interacted with today?");
+        _prompts.Add("Who was the most interesting person I interacted with today, and why?");
         _prompts.Add("What was the best part of my day?");
         _prompts.Add("How did I see the hand of the Lord in my life today?");
         _prompts.Add("What was the strongest emotion I felt today?");
